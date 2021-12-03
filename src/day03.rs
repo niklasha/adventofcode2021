103dcf6939db0cfd63213cfce2ebcc37eb302db6
niklas <niklas@appli.se>
2021-12-03 14:48:23 +0100

    More optimizations and general sanitization.

--- src/day03.rs
+++ src/day03.rs
@@ -23,20 +23,13 @@ impl Day03 {
     fn part1_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
         let dr = Utils::byte_matrix(input)?;
         let cs = dr[0].len();
-        let (c0, c1) = dr.iter().fold(
-            (vec![0 as Output; cs], vec![0 as Output; cs]), |(c0, c1), r| {
-                let c0: Vec<Output> = c0.iter().zip(r.iter()).map(|(c, v)|
-                    if *v == b'0' { *c + 1 } else { *c })
-                    .collect::<Vec<_>>();
-                let c1: Vec<Output> = c1.iter().zip(r.iter()).map(|(c, v)|
-                    if *v == b'1' { *c + 1 } else { *c })
-                    .collect::<Vec<_>>();
-                (c0, c1)
-            });
+        let rs = dr.len() as Output;
+        let c0 = dr.iter().fold((vec![0 as Output; cs]),
+            |c0, r| c0.iter().zip(r.iter())
+                .map(|(c, v)| if *v == b'0' { *c + 1 } else { *c }).collect());
         let g = Self::bin_to_dec(
-            &c0.iter().zip(c1.iter())
-                .map(|(c0, c1)| if *c0 > *c1 { b'0' } else { b'1' }).collect())
-            ?;
+            &c0.iter()
+                .map(|c0| if *c0 > rs - *c0 { b'0' } else { b'1' }).collect())?;
         Ok(g * Self::complement(g, cs))
     }
 
@@ -58,20 +51,24 @@ impl Day03 {
     }
 
     fn most_common(v: &Vec<u8>) -> u8 {
-        if v.iter().filter(|&c|
-            *c == b'0').count() > v.len() / 2 { b'0' } else { b'1' }
+        if v.iter().filter(|&c| *c == b'0').count() > v.len() / 2 {
+            b'0'
+        } else {
+            b'1'
+        }
     }
 
     fn least_common(v: &Vec<u8>) -> u8 {
-        if v.iter().filter(|&c|
-            *c == b'0').count() <= v.len() / 2 { b'0' } else { b'1' }
+        if Self::most_common(v) == b'1' { b'0' } else { b'1' }
     }
 
-    fn compute_rating(dr: &Vec<Vec<u8>>, cs: usize, f: fn(&Vec<u8>) -> u8) -> Result<i64, ParseIntError> {
+    fn compute_rating(dr: &Vec<Vec<u8>>, cs: usize, f: fn(&Vec<u8>) -> u8)
+        -> Result<i64, ParseIntError> {
         Self::bin_to_dec(
             &(0..cs).fold_while(dr.clone(), |candidates, i| {
                 let mcb = f(&candidates.iter().map(|r| r[i]).collect());
-                let n = candidates.into_iter().filter(|r| r[i] == mcb).collect::<Vec<_>>();
+                let n = candidates.into_iter().filter(|r| r[i] == mcb)
+                    .collect::<Vec<_>>();
                 if n.len() == 1 { Done(n) } else { Continue(n.clone()) }
             }).into_inner()[0])
     }
