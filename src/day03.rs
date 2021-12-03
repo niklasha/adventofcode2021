de3c9e77038dcf17e11c935f7536793c9018da5f
niklas <niklas@appli.se>
2021-12-03 09:32:54 +0100

    First part of code cleanup: Remove unwrap. Refactor.

--- src/day03.rs
+++ src/day03.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use itertools::FoldWhile::*;
+use std::num::ParseIntError;
 use crate::day::*;
 
 pub struct Day03 {}
@@ -17,61 +20,60 @@ impl Day for Day03 {
 }
 
 impl Day03 {
-    fn bin_to_dec(v: &Vec<u8>) -> i64 {
-        i64::from_str_radix(&v.into_iter().map(|b| *b as char).collect::<String>(), 2).unwrap()
-    }
-
     fn part1_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
-        let dr = &Utils::byte_matrix(input).unwrap();
+        let dr = Utils::byte_matrix(input)?;
         let cs = dr[0].len();
-        let (c0, c1) = dr.iter().fold((vec![0 as Output; cs], vec![0 as Output; cs]), |(c0, c1), r| {
-            let c0: Vec<Output> = c0.iter().zip(r.iter()).map(|(c, v)|
-                if *v == b'0' { *c + 1 } else { *c })
-                .collect::<Vec<_>>();
-            let c1: Vec<Output> = c1.iter().zip(r.iter()).map(|(c, v)|
-                if *v == b'1' { *c + 1 } else { *c })
-                .collect::<Vec<_>>();
-            (c0, c1)
-        });
-        let g = i64::from_str_radix(
-            &c0.iter().zip(c1.iter()).map(|(c0, c1)| if *c0 > *c1 { '0' } else { '1' }).into_iter().collect::<String>(), 2).unwrap();
-        let e = i64::from_str_radix(
-            &c0.iter().zip(c1.iter()).map(|(c0, c1)| if *c1 > *c0 { '0' } else { '1' }).into_iter().collect::<String>(), 2).unwrap();
+        let (c0, c1) = dr.iter().fold(
+            (vec![0 as Output; cs], vec![0 as Output; cs]), |(c0, c1), r| {
+                let c0: Vec<Output> = c0.iter().zip(r.iter()).map(|(c, v)|
+                    if *v == b'0' { *c + 1 } else { *c })
+                    .collect::<Vec<_>>();
+                let c1: Vec<Output> = c1.iter().zip(r.iter()).map(|(c, v)|
+                    if *v == b'1' { *c + 1 } else { *c })
+                    .collect::<Vec<_>>();
+                (c0, c1)
+            });
+        let g = Self::bin_to_dec(
+            &c0.iter().zip(c1.iter())
+                .map(|(c0, c1)| if *c0 > *c1 { b'0' } else { b'1' }).collect())
+            ?;
+        let e = Self::bin_to_dec(
+            &c0.iter().zip(c1.iter())
+                .map(|(c0, c1)| if *c1 > *c0 { b'0' } else { b'1' }).collect())
+            ?;
         Ok(e * g)
     }
 
+    fn part2_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
+        let dr = Utils::byte_matrix(input)?;
+        let cs = dr[0].len();
+        let ogr = Self::compute_rating(&dr, cs, Self::most_common)?;
+        let co2sr = Self::compute_rating(&dr, cs, Self::least_common)?;
+        Ok(ogr * co2sr)
+    }
+
+    fn bin_to_dec(v: &Vec<u8>) -> Result<i64, ParseIntError> {
+        i64::from_str_radix(
+            &v.into_iter().map(|b| *b as char).collect::<String>(), 2)
+    }
+
     fn most_common(v: &Vec<u8>) -> u8 {
-        if v.iter().filter(|&c| *c == b'0').count() > v.len() / 2 { b'0' } else { b'1' }
+        if v.iter().filter(|&c|
+            *c == b'0').count() > v.len() / 2 { b'0' } else { b'1' }
     }
 
     fn least_common(v: &Vec<u8>) -> u8 {
-        if v.iter().filter(|&c| *c == b'0').count() <= v.len() / 2 { b'0' } else { b'1' }
+        if v.iter().filter(|&c|
+            *c == b'0').count() <= v.len() / 2 { b'0' } else { b'1' }
     }
 
-    fn part2_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
-        let dr = Utils::byte_matrix(input).unwrap();
-        let cs = dr[0].len();
-        let (_, ogr) = (0..cs).fold((dr.clone(), None), |(candidates, r), i| {
-            let mcb = Self::most_common(&candidates.iter().map(|r| r[i]).collect());
-            if r == None {
+    fn compute_rating(dr: &Vec<Vec<u8>>, cs: usize, f: fn(&Vec<u8>) -> u8) -> Result<i64, ParseIntError> {
+        Self::bin_to_dec(
+            &(0..cs).fold_while(dr.clone(), |candidates, i| {
+                let mcb = f(&candidates.iter().map(|r| r[i]).collect());
                 let n = candidates.into_iter().filter(|r| r[i] == mcb).collect::<Vec<_>>();
-                (n.clone(), if n.len() == 1 { Some(n[0].clone()) } else { None })
-            } else {
-                (candidates, r)
-            }
-        });
-        let ogr = ogr.map(|v| Self::bin_to_dec(&v));
-        let (_, co2sr) = (0..cs).fold((dr.clone(), None), |(candidates, r), i| {
-            let lcb = Self::least_common(&candidates.iter().map(|r| r[i]).collect());
-            if r == None {
-                let n = candidates.into_iter().filter(|r| r[i] == lcb).collect::<Vec<_>>();
-                (n.clone(), if n.len() == 1 { Some(n[0].clone()) } else { None })
-            } else {
-                (candidates, r)
-            }
-        });
-        let co2sr = co2sr.map(|v| Self::bin_to_dec(&v));
-        Ok(ogr.unwrap() * co2sr.unwrap())
+                if n.len() == 1 { Done(n) } else { Continue(n.clone()) }
+            }).into_inner()[0])
     }
 }
 
