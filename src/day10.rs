f3489da59240b264e3808e9a39b5265e9376d8f5
niklas <niklas@appli.se>
2021-12-10 07:13:02 +0100

    solution to day 10

--- src/day10.rs
+++ src/day10.rs
@@ -18,11 +18,53 @@ impl Day for Day10 {
 
 impl Day10 {
     fn part1_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
-        Err(Box::new(AocError))
+        let inputs = io::BufReader::new(input).lines()
+            .map(|l| l.unwrap().chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let mut st = vec![];
+        Ok(inputs.into_iter().map(|input| input.into_iter().fold(0, |sc, ch| {
+            if sc != 0 { sc } else {
+                match ch {
+                    '(' | '[' | '{' | '<' => { st.push(ch); sc },
+                    ')' => if st.pop().unwrap() == '(' { 0 } else { 3 },
+                    ']' => if st.pop().unwrap() == '[' { 0 } else { 57 },
+                    '}' => if st.pop().unwrap() == '{' { 0 } else { 1197 },
+                    '>' => if st.pop().unwrap() == '<' { 0 } else { 25137 },
+                    _ => sc
+                }
+            }
+        })).sum())
     }
 
     fn part2_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
-        Err(Box::new(AocError))
+        let inputs = io::BufReader::new(input).lines()
+            .map(|l| l.unwrap().chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let mut scores = inputs.iter().flat_map(|input| {
+            let mut st = vec![];
+            if input.into_iter().fold(0, |sc, &ch| {
+                if sc != 0 { sc } else {
+                    match ch {
+                        '(' | '[' | '{' | '<' => {
+                            st.push(ch);
+                            sc
+                        },
+                        ')' => if st.pop().unwrap() == '(' { 0 } else { 3 },
+                        ']' => if st.pop().unwrap() == '[' { 0 } else { 57 },
+                        '}' => if st.pop().unwrap() == '{' { 0 } else { 1197 },
+                        '>' => if st.pop().unwrap() == '<' { 0 } else { 25137 },
+                        _ => sc
+                    }
+                }
+            }) == 0 {
+                st.reverse();
+                Some(st.iter().fold(0, |sc, token| sc * 5 + match token {
+                    '(' => 1, '[' => 2, '{' => 3, '<' => 4, _ => 0
+                }))
+            } else { None }
+        }).collect::<Vec<_>>();
+        scores.sort();
+        Ok(scores[scores.len() / 2])
     }
 }
 
@@ -36,7 +78,18 @@ mod tests {
 
     #[test]
     fn part1() {
-        test1("", 0);
+        test1(
+            "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]",
+            26397);
     }
 
     fn test2(s: &str, f: Output) {
@@ -45,6 +98,17 @@ mod tests {
 
     #[test]
     fn part2() {
-        test2("", 0);
+        test2(
+            "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]",
+            288957);
     }
 }
