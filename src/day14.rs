fa03cd11a90ca59ae02f0bf0022aebeb66132905
niklas <niklas@appli.se>
2021-12-14 08:21:32 +0100

    Solution to day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,4 +1,8 @@
 use crate::day::*;
+use crate::day::io::Read;
+use regex::Regex;
+use std::collections::HashMap;
+use std::iter;
 
 pub struct Day14 {}
 
@@ -18,11 +22,81 @@ impl Day for Day14 {
 
 impl Day14 {
     fn part1_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
-        Err(Box::new(AocError))
+        let (init, rules) = Self::parse(input)?;
+        let polymer = (0..10).fold(init, |polymer, _| {
+            let polymer = polymer.chars().tuple_windows().flat_map(|(a, b)| {
+                match rules.get(([a, b].iter().collect::<String>()).as_str()) {
+                    Some(insert) => vec![a, insert.chars().next().unwrap()],
+                    _ => vec![a]
+                }
+            })
+                .chain(iter::once(polymer.chars().last().unwrap()))
+                .collect::<String>();
+            polymer
+        });
+        let mut counts = HashMap::new();
+        for c in polymer.chars() {
+            counts.entry(c).and_modify(|count| *count += 1).or_insert(1);
+        }
+        let counts = counts.into_iter().sorted_by_key(|&(_, count)| count)
+            .collect::<Vec<_>>();
+        Ok(counts[counts.len() - 1].1 - counts[0].1)
     }
 
     fn part2_impl(self: &Self, input: &mut dyn io::Read) -> BoxResult<Output> {
-        Err(Box::new(AocError))
+        let (init, rules) = Self::parse(input)?;
+        let rules = rules.into_iter().map(|(k, v)| {
+                let (left, right)
+                    = k.chars().tuples::<(_, _)>().next().unwrap();
+                let insert = v.chars().next().unwrap();
+                ((left, right), [(left, insert), (insert, right)])
+            }).collect::<HashMap<_, _>>();
+        let mut counts = HashMap::new();
+        init.chars().for_each(|c| {
+            counts.entry(c).and_modify(|count| *count += 1).or_insert(1);
+        });
+        let mut pair_counts = HashMap::new();
+        init.chars().tuple_windows::<(_, _)>()
+            .filter(|pair| rules.contains_key(pair)).for_each(|pair| {
+                pair_counts.entry(pair).and_modify(|count| *count += 1)
+                    .or_insert(1);
+        });
+        (0..40).fold(pair_counts, |pair_counts, _| {
+            let mut new_pair_counts = HashMap::new();
+            pair_counts.iter().for_each(|(pair, &pair_count)| {
+                let new_pairs = rules.get(&pair).unwrap();
+                let insert = new_pairs[0].1;
+                new_pairs.iter().for_each(|&pair| {
+                    new_pair_counts.entry(pair)
+                        .and_modify(|count| *count += pair_count)
+                        .or_insert(pair_count);
+                });
+                counts.entry(insert).and_modify(|count| *count += pair_count)
+                    .or_insert(pair_count);
+            });
+            new_pair_counts
+        });
+        let counts = counts.into_iter().sorted_by_key(|&(_, count)| count)
+            .collect::<Vec<_>>();
+        Ok(counts[counts.len() - 1].1 - counts[0].1)
+    }
+
+    fn parse(input: &mut dyn Read) -> BoxResult<(String, HashMap<String, String>)> {
+        let mut lines = io::BufReader::new(input).lines();
+        let init = lines.next().ok_or(AocError)??;
+        lines.next().ok_or(AocError)??;
+        let rules = lines.map(|line| line.map_err(|e| e.into())
+            .and_then(Self::parse_insertion))
+            .map(|r| r.unwrap()).collect::<HashMap<_, _>>();
+        Ok((init, rules))
+    }
+
+    fn parse_insertion(s: String) -> BoxResult<(String, String)> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new("(\\w+) -> (\\w+)").unwrap();
+        }
+        let cap = RE.captures(&s).ok_or(AocError)?;
+        Ok((cap[1].to_string(), cap[2].to_string()))
     }
 }
 
@@ -36,7 +110,24 @@ mod tests {
 
     #[test]
     fn part1() {
-        test1("", 0);
+        test1("NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C", 1588);
     }
 
     fn test2(s: &str, f: Output) {
@@ -45,6 +136,23 @@ mod tests {
 
     #[test]
     fn part2() {
-        test2("", 0);
+        test2("NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C", 2188189693529);
     }
 }
